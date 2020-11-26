537c3349762a67305de17128f5a3f2101a27203b
zitexx <zitexx@gmail.com>
2020-11-26 21:55:04 +0200

    mixitup 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
 $(function () {
 
-  var mixer = mixitup('.gallery__product');
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 })
 
 $(".slider__inner").slick({
